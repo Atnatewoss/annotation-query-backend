f379d9d4382e2de2e6564ae5f1f011a64f4a11a9
Atnatewoss <icodeyt21@gmail.com>
2026-04-16 14:30:25 +0300

    feat(native): export GraphProcessor methods to Python via PyBind11

--- native/graph_module.cpp
+++ native/graph_module.cpp
@@ -42,3 +42,58 @@ Graph py_to_graph(const py::dict& obj) {
     return g;
 }
 
+py::dict graph_to_py_wrapped(const Graph& g) {
+    return convert_to_graph_json(g, true);
+}
+
+PYBIND11_MODULE(graph_native, m) {
+    m.def("group_node_only", [](py::dict g, py::dict r) {
+        return graph_to_py_wrapped(group_node_only(py_to_graph(g), r));
+    });
+    
+    m.def("get_node_to_connections_map", [](py::dict g) {
+        auto [mapping, nodes] = get_node_to_connections_map(py_to_graph(g));
+        return make_pair(mapping, nodes);
+    });
+    
+    m.def("collapse_nodes", [](py::dict g) {
+        return graph_to_py_wrapped(collapse_nodes(py_to_graph(g)));
+    });
+    
+    m.def("collapse_node_nx", [](py::dict g) {
+        return graph_to_py_wrapped(collapse_node_nx(py_to_graph(g)));
+    });
+    
+    m.def("convert_to_graph_json", [](py::dict g, bool allow_data) {
+        return convert_to_graph_json(py_to_graph(g), allow_data);
+    });
+    
+    m.def("group_into_parents", [](py::dict g) {
+        return graph_to_py_wrapped(group_into_parents(py_to_graph(g)));
+    });
+    
+    m.def("group_graph", [](py::dict g) {
+        return graph_to_py_wrapped(group_graph(py_to_graph(g)));
+    });
+    
+    m.def("break_grouping", [](py::dict g) {
+        return graph_to_py_wrapped(break_grouping(py_to_graph(g)));
+    });
+    
+    m.def("collapse_node_nx_location", [](py::dict g) {
+        return graph_to_py_wrapped(collapse_node_nx_location(py_to_graph(g)));
+    });
+    
+    m.def("build_graph_nx", [](py::dict g) {
+        // Since we just build internal representation, and wrapper uses it for other things,
+        // we just return the graph dict for now. Wrapper can convert to nx if needed.
+        return g; 
+    });
+    
+    m.def("build_subgraph_nx", [](py::dict g) {
+        auto components = build_subgraph_nx(py_to_graph(g));
+        py::list res;
+        for (const auto& cg : components) res.append(graph_to_py_wrapped(cg));
+        return res;
+    });
+}
