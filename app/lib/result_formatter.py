6092b32089de2863a3f96fba5bba390192ed38bf
atnatewoss <icodeyt21@gmail.com>
2026-04-26 13:13:35 -0700

    chore: define the current data format

---
+++ app/lib/result_formatter.py
@@ -0,0 +1,22 @@
+
+class Result_Formatter:
+    # Step-1: define the data input and output formats of all three of them
+    raw_data_format = {
+        "neo4j": {
+            "input": "",
+            "output": ""
+        },
+        "mork": {
+            "input": "",
+            "output": ""
+        },
+        "metta": {
+            "input": "",
+            "output": ""
+        },
+    }
+
+    normalized_data_format = {
+        "input": "",
+        "output": ""
+    }
